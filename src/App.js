6d1e17170533f917d556d400c2d1439c35cf2967
Jorge Luis Bou-saad <jbousing@gmail.com>
2020-07-29 23:33:26 -0400

    crop terminado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Crop from './components/Crop';
 
 function App() {
   return (
@@ -19,6 +20,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div>
+        <Crop></Crop>
+      </div>
     </div>
   );
 }
