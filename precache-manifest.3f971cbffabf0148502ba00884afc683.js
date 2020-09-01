9fe311d00995f16af62e20e4d7d0298df8089772
Jorge Luis Bou-saad <jbousing@gmail.com>
2020-09-01 00:25:13 -0400

    se hace build para mostrar experimento con gh-pages

---
+++ precache-manifest.3f971cbffabf0148502ba00884afc683.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a46b9fc5a3790f684b78c2a50aa1929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0066bb51caed193885fa",
+    "url": "/static/css/2.cbeaa07e.chunk.css"
+  },
+  {
+    "revision": "065596e84635e69ca71c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0066bb51caed193885fa",
+    "url": "/static/js/2.1f06c5ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f06c5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065596e84635e69ca71c",
+    "url": "/static/js/main.b48bfc51.chunk.js"
+  },
+  {
+    "revision": "8af95f028a2c2533e162",
+    "url": "/static/js/runtime-main.3df92518.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
